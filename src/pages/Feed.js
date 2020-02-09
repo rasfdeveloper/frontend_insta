c4edf94c8d4e7e1ba4fdf8cc50aee7585bdc718c
Renato Franco <renato.franco@eticasolucoes.com.br>
2020-02-09 18:32:52 -0200

    fixes

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,60 +1,60 @@
-import React, { Component } from 'react';
-import api from '../services/api';
+import React, { Component } from "react";
+import api from "../services/api";
 
-import './Feed.css';
+import "./Feed.css";
 
-import more from '../assets/more.svg';
-import like from '../assets/like.svg';
-import comment from '../assets/comment.svg';
-import send from '../assets/send.svg';
+import more from "../assets/more.svg";
+import like from "../assets/like.svg";
+import comment from "../assets/comment.svg";
+import send from "../assets/send.svg";
 
 class Feed extends Component {
-    state = {
-        feed: [],
-    };
+  state = {
+    feed: []
+  };
 
-    async componentDidMount() {
-        const response = await api.get('posts');
+  async componentDidMount() {
+    const response = await api.get("posts");
 
-        this.setState({ feed: response.data});
+    this.setState({ feed: response.data });
+  }
 
-    }
+  render() {
+    return (
+      <section id="post-list">
+        {this.state.feed.map(post => (
+          <article key={post._id}>
+            <header>
+              <div className="user-info">
+                <span>{post.author}</span>
+                <span className="place">{post.place}</span>
+              </div>
 
-    render(){
-        return (
-            <section id="post-list">
-                { this.state.feed.map(post => (
-                    <article key={post._id}>
-                    <header>
-                        <div className="user-info">
-                            <span>{post.author}</span>
-                            <span className="place">{post.place}</span>
-                        </div>
+              <img src={more} alt="Mais" />
+            </header>
 
-                        <img src={more} alt="Mais"/>
-                    </header>
+            <img src={`http://3.19.79.117:3333/files/${post.image}`} alt="" />
 
-                    <img src={`http://localhost:8080/files/${post.image}`} alt=""/>
+            <footer>
+              <div className="actions">
+                <button type="button" onClick={this.handleLike} />
+                <img src={like} alt="" />
+                <img src={comment} alt="" />
+                <img src={send} alt="" />
+              </div>
 
-                    <footer>
-                        <div className="actions">
-                        <button type="button" onClick={this.handleLike}/>
-                            <img src={like} alt=""/>
-                            <img src={comment} alt=""/>
-                            <img src={send} alt=""/>
-                        </div>
+              <strong>{post.likes} curtidas</strong>
 
-                        <strong>{post.likes} curtidas</strong>
-
-                        <p>{post.description}
-                            <span>{post.hashtags}</span>
-                        </p>
-                    </footer>
-                </article>
-                ))}
-            </section>
-        );
-    }
+              <p>
+                {post.description}
+                <span>{post.hashtags}</span>
+              </p>
+            </footer>
+          </article>
+        ))}
+      </section>
+    );
+  }
 }
 
 export default Feed;
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080',
-})
+  baseURL: "http://3.19.79.117:3333"
+});
 
 export default api;
